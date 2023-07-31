bbd2b1ba2d3c5804946f656cd4cc88b15ce9e971
Bedrock-the-ninth <ravi.be2002@gmail.com>
2023-07-31 17:44:25 +0330

    Removed unnecessary global variables, renamed functions to be more descriptive, simplified user input handling, replace depricated while loops with time.sleep() to help with processor functioning, added comments for more readability and also error handling to create more comprehensible errors.

--- remindeer.py
+++ remindeer.py
@@ -1,13 +1,16 @@
+# Imports
 from win10toast import ToastNotifier
 import time
 import math
 
 
+# This function sends notifications to the Windows user (a toast notification)
 def send_notification(title, message):
     toaster = ToastNotifier()
     toaster.show_toast(title, message, duration=10)
 
 
+# This fetches the user's initial input to indicate if they want to start the script
 def usr_decision():
     usr_inp = input("Do you want to keep the record of your working duration?(Y/n): ")
     if usr_inp.lower() == "y":
@@ -17,78 +20,104 @@ def usr_decision():
 
 
 if __name__ == "__main__":
+    # This function checks if the user wants to run the script
+    #  in the first place and if not it will exit the program
     first_decision = usr_decision()
     if first_decision == True:
         pass
     else:
         exit()
 
+    # This function is supposed to fetch the variables for rest and work time
     def get_time():
-        global work_time, rest_time
-        work_time = float(input("How often do you want to be reminded (per minutes)? "))
-        rest_time = float(
-            input("How much rest after each set do you need (per minutes)? ")
-        )
+        while True:
+            try:
+                work_time = float(
+                    input("In how many minutes do you want to get some rest? ")
+                )
+                if work_time <= 0:
+                    raise ValueError("Work time must be greater than zero.")
+                rest_time = float(
+                    input("How much rest do you require after it (in minutes)? ")
+                )
+                if rest_time < 0:
+                    raise ValueError("Rest time cannot be negative.")
+                return work_time, rest_time
+            except ValueError as e:
+                print(f"Invalid input: {e}")
 
-    def work_hours_counter():
-        global work_time_finished
-        elapsed_time_mins = 0
-        start_time = time.time()
-        work_time_finished = False
-        while elapsed_time_mins != work_time:
-            elapsed_time_mins = (time.time() - start_time) / 60
-            if elapsed_time_mins >= work_time:
+    # Works like a timer and when counter riches the variable defined by the user
+    # , it notifies the user
+    def work_duration_counter(wt):
+        try:
+            elapsed_work_mins = 0
+            start_time = time.time()
+
+            time.sleep(wt * 60)
+
+            elapsed_work_mins = (time.time() - start_time) / 60
+
+            print(
+                f"You have been working for {elapsed_work_mins:.2f} minutes, take some rest now!"
+            )
+            send_notification(
+                "REST TIME!",
+                f"You have been working for {elapsed_work_mins:.2f} minutes, take some rest now!",
+            )
+
+            return True
+        except ZeroDivisionError:
+            print("Error: Work time cannot be zero.")
+            return False
+
+    # Immediately after the work_time_counter, this function starts working
+    # , which keeps track of how many minutes the user wanted to rest and notifies them
+    def rest_duration_counter(rt, wtf):
+        try:
+            if wtf:
+                rest_time_start = time.time()
+                time.sleep(rt * 60)
+                elapsed_rest_mins = (time.time() - rest_time_start) / 60
                 print(
-                    f"You have been working for {elapsed_time_mins} minutes, take some rest now!"
+                    f"You have been resting for {elapsed_rest_mins:.2f} minutes, get back to work!"
                 )
                 send_notification(
-                    "REST TIME!",
-                    f"You have been working for {math.ceil(elapsed_time_mins)} minutes, take some rest now!",
+                    "GET BACK TO WORK!",
+                    f"You have been resting for {elapsed_rest_mins:.2f} minutes, get back to work!",
                 )
-                work_time_finished = True
-                break
-            else:
-                pass
 
-    def rest_time_counter():
-        global usr_last_inp
-        if work_time_finished == True:
-            rest_time_start = time.time()
-            elapsed_rest_mins = 0
-            while elapsed_rest_mins != rest_time:
-                elapsed_rest_mins = (time.time() - rest_time_start) / 60
-                if elapsed_rest_mins >= rest_time:
-                    print(
-                        f"You have been resting for {elapsed_rest_mins} minutes, get back to work!"
-                    )
-                    send_notification(
-                        "GET BACK TO WORK!",
-                        f"You have been resting for {math.ceil(elapsed_rest_mins)} minutes, get back to work!",
-                    )
-                    break
-                else:
-                    pass
-        usr_last_inp = input(
-            "Do you want to exit the program (e),\nredefine the work and rest time variables and go again (r)\nor simply restart the time with the previously set durations (p)? "
-        ).lower()
+            usr_last_inp = input(
+                "Do you want to\nexit (e)\nredefine variables (r)\nor restart with previous variables (p)? "
+            ).lower()
 
+            return usr_last_inp
+        except KeyboardInterrupt:
+            print("Operation interrupted. Exiting...")
+            exit()
+
+    # Immediately after the rest_time_counter, this function runs to fetch the
+    # user's input for terminating the script or keeping it running
+    def last_inp_logic(inp):
+        if inp == "e":
+            exit()
+        elif inp == "r":
+            overall_process()
+        elif inp == "p":
+            work_duration_counter()
+            rest_duration_counter()
+            last_inp_logic()
+        else:
+            exit()
+
+    # All the logic that has been implemented runs via this function
     def overall_process():
-        get_time()
-        work_hours_counter()
-        rest_time_counter()
+        wt, rt = get_time()
+        wtf = work_duration_counter(wt)
+        usr_last_inp = rest_duration_counter(rt, wtf)
+        last_inp_logic(usr_last_inp)
 
-        def last_inp_logic():
-            if usr_last_inp == "e":
-                exit()
-            elif usr_last_inp == "r":
-                overall_process()
-            elif usr_last_inp == "p":
-                work_hours_counter()
-                rest_time_counter()
-                last_inp_logic()
-            else:
-                exit()
-
-        last_inp_logic()
-
-    overall_process()
+    # Where all the magic comes to life :)
+    try:
+        overall_process()
+    except KeyboardInterrupt:
+        print("Operation interrupted. Exiting...")
